a594c0ad644bde6208a7ec4bf9b44603b3fe977c
mokshanirgutti <mokshasai910@gmail.com>
2024-12-11 06:09:05 +0530

    update

--- regerster/scripts.js
+++ regerster/scripts.js
@@ -6,16 +6,12 @@
     
     const email = document.getElementById('regEmail').value.trim();
     const pwd = document.getElementById('regPassword').value.trim();
-    const phone = document.getElementById('phoneNumber').value.trim(); // No Number conversion here
+    const phone = document.getElementById('phoneNumber').value.trim();
     
     const regError = document.getElementById('regError');
-    // console.log(first, last, email, pwd, phone, dob);
+    
     const no = Number(phone);
-    console.log( typeof phone);
-    console.log(typeof no) ;
-    console.log(phonePattern.test(no));
-    console.log(phonePattern.test(phone));
-
+    
     regError.textContent =  !emailPattern.test(email) ? 'Invalid Email' :
       pwd.length < 6 ? 'Password too short' :
     !phonePattern.test(no) ? 'Invalid Phone' : 'Registration Successful';
